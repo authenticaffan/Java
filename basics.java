c39026d684c2297327ae030248fb438b94234dd4
Mohamed Affan <mohamedaffan477@gmail.com>
2024-04-02 18:28:01 +0000

    # new file:   basics.class 	# new file:   basics.java     # new file:   helloworld.java

---
+++ basics.java
@@ -0,0 +1,16 @@
+public class basics{
+    public static void main(String [] args){
+        // char ini='K';
+        // String name=" Mohamed Affan ";
+        // int age= 20 ;
+        // double weight= 76.50 ;
+        // boolean indian= true ;
+        // System.out.println(ini+name+age+weight+indian);
+        // Type Casting
+        // widening casting
+        int a=10;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
