63a3760ac34ad9d62a3e6bcdc15d961c48b4ee6e
Mohamed Affan <mohamedaffan477@gmail.com>
2024-04-03 18:55:38 +0000

    today's learning

--- basics.java
+++ basics.java
@@ -8,9 +8,47 @@ public class basics{
         // System.out.println(ini+name+age+weight+indian);
         // Type Casting
         // widening casting
-        int a=10;
-        float b=a;
-        System.out.println(a);
-        System.out.println(b);
+        // int a=10;
+        // float b=a;
+        // System.out.println(a);
+        // System.out.println(b);
+        //String
+        //String fnam="Md";
+        //String lnam=" Affan";
+        //System.out.println("My Name is "+nam);
+        //System.out.println("length of nam is "+nam.length());
+        //similarly toUpperCase() toLowerCase() indexOf()
+        //System.out.println(fnam+" "+lnam);
+        //System.out.println(fnam.concat(lnam));
+        // System.out.println("Myself \'Mohamed Affan\'\n");
+        // System.out.println("Myself \"Mohamed Affan\"\n");
+        // System.out.println("Myself \\Mohamed Affan\\\n");
+        //now lets see if-else
+        // int a=10;
+        // int b=20;
+        // if(a==b){
+        //     System.out.println("True");
+        // }
+        // else{
+        //     System.out.println("False");
+        // }
+        // String check=(a==b)?"True":"False";
+        // System.out.println(check);
+        // int a=1;
+        // switch (a){
+        //     case 1:
+        //         System.out.println("sigma");
+        //         break;
+        //     case 2:
+        //         System.out.println("giga");
+        //     default:
+        //         System.out.println("human");    
+        // }
+        //similarly while,do while, for
+        //for each
+        //     int[] a={10,20,30};
+        // for(int b:a){
+        //     System.out.println(b);
+        // } 
     }
 }
